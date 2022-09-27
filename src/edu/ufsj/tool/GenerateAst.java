39c027a3e46472d539af1131aa64b62add908363
Guilherme Paiva <38262537+Senpaiva@users.noreply.github.com>
2022-09-27 14:40:07 -0300

    Update GenerateAst.java

--- src/edu/ufsj/tool/GenerateAst.java
+++ src/edu/ufsj/tool/GenerateAst.java
@@ -26,9 +26,30 @@ public class GenerateAst {
             writer.println();
             writer.println("import java.util.List;");
             writer.println();
-            writer.println("class " + baseName + "(");
+            writer.println("class " + baseName + "{");
+            
+            for(String type: types){
+                String className = type.split(":")[0].trim();
+                String fields = type.split(":")[1].trim();
+                defineType(writer, baseName, className, fields);
+            }
 
-            writer.println(")");
+            writer.println("}");
             writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList){
+        writer.println("   static class " + className + " extends " + baseName + " {"); //construtor
+        writer.println("     " + className + "(" + fieldList + ") {");
+        String[] fields = fieldList.split(", ");
+        for (String field : fields){
+            String name = field.split(" ")[1];
+            writer.println("       this." + name + " = " + name + ";");
+        }
+        writer.println("      }");
+        writer.println();
+        for (String field: fields){
+            writer.println("   final " + field + ";");
+        }
     }
 }
